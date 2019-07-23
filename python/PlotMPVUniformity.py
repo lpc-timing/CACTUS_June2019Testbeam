e22001040ed75c0a19f9aceebd28565e05151fa7
Si Xie <si.xie@cern.ch>
2019-07-23 11:18:52 -0500

    add capability to make MPV plot for each pixel

--- python/PlotMPVUniformity.py
+++ python/PlotMPVUniformity.py
@@ -1,9 +1,35 @@
 from ROOT import TFile,TTree,TCanvas,TH1F,TH2F,TLatex,TMath,TEfficiency,TGraphErrors
 import os
 import array
+import sys
+
+inputfile_5_3 = "/eos/uscms/store/user/cmstestbeam/2019_04_April_CMSTiming/KeySightScope/RecoData/TimingDAQRECO/RecoWithTracks/v6_CACTUSSkim/Completed/Data_CACTUSAnalog_Pixel5_3_16216-16263.root"
+inputfile_5_4 = "/eos/uscms/store/user/cmstestbeam/2019_04_April_CMSTiming/KeySightScope/RecoData/TimingDAQRECO/RecoWithTracks/v6_CACTUSSkim/Completed/Data_CACTUSAnalog_Pixel5_4_17063-17362.root"
+inputfile_5_10 = "/eos/uscms/store/user/cmstestbeam/2019_04_April_CMSTiming/KeySightScope/RecoData/TimingDAQRECO/RecoWithTracks/v6_CACTUSSkim/Completed/Data_CACTUSAnalog_Pixel5_10_16725-17039.root"
+inputfile = ""
+outputLabel = ""
+pixelName = ""
+
+if (len(sys.argv) < 2):
+    print "Please give an argument to select the dataset: (1) Pixel 5_3 data , (2) Pixel 5_4 data, (3) Pixel 5_10 data."
+    exit()
+
+if (sys.argv[1] == "1"):
+    inputfile = inputfile_5_3
+    outputLabel = "_Pixel_5_3"
+    pixelName = "(5,3)"
+
+if (sys.argv[1] == "2"):
+    inputfile = inputfile_5_4
+    outputLabel = "_Pixel_5_4"
+    pixelName = "(5,4)"
+
+if (sys.argv[1] == "3"):
+    inputfile = inputfile_5_10
+    outputLabel = "_Pixel_5_10"
+    pixelName = "(5,10)"
 
 
-inputfile = "/eos/uscms/store/user/cmstestbeam/2019_04_April_CMSTiming/KeySightScope/RecoData/TimingDAQRECO/RecoWithTracks/v6_CACTUSSkim/Completed/Data_CACTUSAnalog_HighGain_16719-16722+17040-17062.root"
 
 if not os.path.exists(inputfile):
     print "input file "+inputfile+" does not exist"
@@ -82,14 +108,14 @@ effGraph.GetYaxis().SetTitle("Amplitude MPV [mV]")
 effGraph.GetYaxis().SetTitleSize(0.05)
 effGraph.GetYaxis().SetTitleOffset(0.92)
 effGraph.GetYaxis().SetLabelSize(0.03)
-effGraph.GetYaxis().SetRangeUser(15.0,25.0)
+effGraph.GetYaxis().SetRangeUser(10.0,25.0)
 
 title = TLatex()
 title.SetTextSize(0.05);
 #title.SetTextAlign(13);  
-title.DrawLatexNDC(.2,.93,"CACTUS Pixel (5,3) Analog");
+title.DrawLatexNDC(.2,.93,"CACTUS Pixel "+pixelName+" Analog");
 c.Update()
-c.SaveAs("CACTUS_MPVVsX.gif")
+c.SaveAs("CACTUS_MPVVsX" + outputLabel + ".gif")
 
 
 
@@ -150,14 +176,82 @@ effGraph.GetYaxis().SetTitle("Amplitude MPV [mV]")
 effGraph.GetYaxis().SetTitleSize(0.05)
 effGraph.GetYaxis().SetTitleOffset(0.92)
 effGraph.GetYaxis().SetLabelSize(0.03)
-effGraph.GetYaxis().SetRangeUser(15.0,25.0)
+effGraph.GetYaxis().SetRangeUser(10.0,25.0)
 
 title = TLatex()
 title.SetTextSize(0.05);
 #title.SetTextAlign(13);  
-title.DrawLatexNDC(.2,.93,"CACTUS Pixel (5,3) Analog");
+title.DrawLatexNDC(.2,.93,"CACTUS Pixel ("+pixelName+" Analog");
 c.Update()
-c.SaveAs("CACTUS_MPVVsY.gif")
+c.SaveAs("CACTUS_MPVVsX" + outputLabel + ".gif")
+
+
+
+
+
+##########################
+#1D MPV Vs Y
+##########################
+yLeftBoundary = 23.0
+yRightBoundary = 25.0
+yBinWidth = 0.1
+yNBins = int((yRightBoundary - yLeftBoundary)/yBinWidth)
+
+
+tmpHist2D = TH2F("tmpHist2D",";Y [mm];Amplitude [mV]; NEvt", yNBins, yLeftBoundary, yRightBoundary, 50, 0, 100)
+
+tree.Draw("amp[3]:y_dut[2] >> tmpHist2D","(y_dut[2] > 23.50 && y_dut[2] < 24.0 && x_dut[2] > 19.6 && x_dut[2] < 20.4) && amp[0] > 150 && amp[0] < 600 && ( (t_peak[3] - t_peak[0])*1e9 > 0 && (t_peak[3] - t_peak[0])*1e9 < 25)","colz")
+
+y = list()
+yErr = list()
+mpv = list()
+mpvErr = list()
+nbins = tmpHist2D.GetXaxis().GetNbins()
+
+for b in range(1,tmpHist2D.GetXaxis().GetNbins()+1):
+    projectionHist = tmpHist2D.ProjectionY("proj"+str(b), b,b)
+
+    y.append( tmpHist2D.GetXaxis().GetBinCenter(b))
+    yErr.append( tmpHist2D.GetXaxis().GetBinCenter(b) - tmpHist2D.GetXaxis().GetBinLowEdge(b))
+    tmpMPV = -1
+    tmpMPVErr = 0
+
+    #skip bins with too few events
+    if (projectionHist.Integral() > 100) : 
+
+        fitresult = projectionHist.Fit("landau","S")
+        tmpMPV = fitresult.Parameter(1)
+        tmpMPVErr = fitresult.ParError(1)
+ 
+    mpv.append(tmpMPV)
+    mpvErr.append(tmpMPVErr)
+
+
+xArr = array.array('f',x)
+xErrArr = array.array('f',xErr)
+yArr = array.array('f',mpv)
+yErrArr = array.array('f',mpvErr)
+
+effGraph = TGraphErrors(nbins, xArr, yArr, xErrArr, yErrArr );
+effGraph.Draw("APE")
+effGraph.SetTitle("")
+effGraph.GetXaxis().SetTitle("Y [mm]")
+effGraph.GetXaxis().SetTitleSize(0.05)
+effGraph.GetXaxis().SetTitleOffset(0.90)
+effGraph.GetXaxis().SetLabelSize(0.03)
+effGraph.GetXaxis().SetRangeUser(23.0,25.0)
+effGraph.GetYaxis().SetTitle("Amplitude MPV [mV]")
+effGraph.GetYaxis().SetTitleSize(0.05)
+effGraph.GetYaxis().SetTitleOffset(0.92)
+effGraph.GetYaxis().SetLabelSize(0.03)
+effGraph.GetYaxis().SetRangeUser(10.0,25.0)
+
+title = TLatex()
+title.SetTextSize(0.05);
+#title.SetTextAlign(13);  
+title.DrawLatexNDC(.2,.93,"CACTUS Pixel "+pixelName+" Analog");
+c.Update()
+c.SaveAs("CACTUS_MPVVsY" + outputLabel + ".gif")
 
 
 
